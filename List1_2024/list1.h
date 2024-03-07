45ff6d65ab7727091fde45418efc25935038c180
Sergey Makletsov <smaklets@kpfu.ru>
2024-03-07 12:03:41 +0300

    Лекция от 7.03

--- List1_2024/list1.h
+++ List1_2024/list1.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdint.h>
 
 template <typename T>
 struct list1_elem
@@ -8,33 +9,119 @@ struct list1_elem
 };
 
 template <typename T>
-void add(list1_elem<T>*& first, T value)
+void insert(list1_elem<T>*& first, T value, int index)
 {
 	list1_elem<T>* new_el = new list1_elem<T>;
 	new_el->data = value;
-	if (!first) first = new_el;
+	if (!first || index <= 0) 
+	{
+		new_el->next = first;
+		first = new_el;
+	}
 	else
 	{
 		auto* curr = first;
-		while (curr->next) curr = curr->next;
+		int curr_i = 0;
+		while (curr->next && ++curr_i < index) curr = curr->next;
+		new_el->next = curr->next;
 		curr->next = new_el;
 	}
 }
 
 template <typename T>
-void insert(list1_elem<T>*& first, T value, int index);
+void add(list1_elem<T>*& first, T value)
+{
+	insert(first, value, INT_FAST32_MAX);
+}
 
 template <typename T>
-T get(list1_elem<T>* first, int index);
+bool get(list1_elem<T>* first, int index, T& result)
+{
+	if (index < 0 || !first) return false;
+	auto curr = first;
+	auto i = 0;
+	while (curr && i++ < index)
+	{
+		curr = curr->next;
+	}
+	if (!curr) return false;
+	result = curr->data;
+	return true;
+}
 
 template <typename T>
-void remove(list1_elem<T>*& first, T value);
+void remove(list1_elem<T>*& first, T value)
+{
+	if (!first) return;
+	auto curr = first;
+	if (first->data == value)
+	{
+		first = first->next;
+		delete curr;
+	}
+	else
+	{
+		while (curr->next && curr->next->data != value)
+		{
+			curr = curr->next;
+		}
+		if (curr->next)
+		{
+			auto del = curr->next;
+			curr->next = curr->next->next;
+			delete del;
+		}
+	}
+}
 
 template <typename T>
-void removeAll(list1_elem<T>*& first, T value);
+void removeAll(list1_elem<T>*& first, T value)
+{
+	if (!first) return;
+	auto curr = first;
+	while (first && first->data == value)
+	{
+		first = first->next;
+		delete curr;
+		curr = first;
+	}
+	do {
+		while (curr->next && curr->next->data != value)
+		{
+			curr = curr->next;
+		}
+		if (curr->next)
+		{
+			auto del = curr->next;
+			curr->next = curr->next->next;
+			delete del;
+		}
+	} while (curr->next);
+}
 
 template <typename T>
-void removeAt(list1_elem<T>*& first, int index);
+void removeAt(list1_elem<T>*& first, int index)
+{
+	if (index < 0 || !first) return;
+	auto curr = first;
+	auto i = 0;
+	if (index == 0)
+	{
+		auto del = first;
+		first = first->next;
+		delete del;
+	}
+	else {
+		while (curr->next && ++i < index)
+		{
+			curr = curr->next;
+		}
+		if (!curr->next) return;
+		auto del = curr->next;
+		curr->next = curr->next->next;
+		delete del;
+	}
+}
 
 template <typename T>
 void clear(list1_elem<T>*& first)
--- List1_2024/main.cpp
+++ List1_2024/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-
+#include "list1.h"
 using namespace std;
+
+template <typename T>
+void show_list(list1_elem<T>* list)
+{
+	auto curr = list;
+	while (curr)
+	{
+		cout << curr->data << " -> ";
+		curr = curr->next;
+	}
+	cout << endl;
+}
 
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	size_t sz = 7;
+	int* a = new int[]{ 1, 3, 3, 4, 5, 4, 9 };
+	list1_elem<int>* list = nullptr;
+	for (int i = 0; i < sz; i++)
+	{
+		add(list, a[i]);
+	}
+	show_list(list);
+	insert(list, -2, -100);
+	insert(list, 2, 0);
+	insert(list, -2,4);
+	insert(list, -2, 100);
+	show_list(list);
+	clear(list);
+	delete[] a;
 }
